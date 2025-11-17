d354e128f1510bd11c10dd5927ff2fb72d5c9041
Yevhen Lebid <ealebed@gmail.com>
2025-11-17 19:12:56 +0200

    test

--- vanilla-ga/terraform/envs/prod/variables.tf
+++ vanilla-ga/terraform/envs/prod/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "environment" {
   type        = string
   description = "Deployment environment"
-  default     = "prod"
+  # default     = "prod"
 }
 
 variable "project_name" {
